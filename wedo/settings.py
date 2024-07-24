c0d82b9f2f52d7c198d4c1cee55de224237483b0
Mathan <mathanmahe99@gmail.com>
2024-07-23 22:02:38 -0400

    update db to postgres

--- wedo/settings.py
+++ wedo/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'wedo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# import dj_database_url
-# import os
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 
 AUTH_USER_MODEL = "tasks.User"
